f70baad8fd0d5f0ed94812c4a4bcbef589d64cea
IdyllFatima <idil.f.yilm>
2021-07-20 21:19:48 +0300

    Initial Commit

---
+++ weOcean/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weOcean
+//
+//  Created by İlhan yılmaz on 20.07.2021.
+//  Copyright © 2021 Idyll Fatima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
